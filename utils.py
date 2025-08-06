20c026cf5bc94ad158bfb3241a63298d9fdc4b3f
RajatDeore7 <rajat.72002@gmail.com>
2025-08-07 00:20:32 +0530

    Added Env

--- utils.py
+++ utils.py
@@ -1,8 +1,11 @@
-# S3 storage utility
 import boto3
 import json
 import os
 from datetime import datetime
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
 
 s3 = boto3.client(
     "s3",
@@ -11,14 +14,14 @@ s3 = boto3.client(
     region_name="eu-central-1",
 )
 
-S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
+bucket_name = os.getenv("S3_BUCKET_NAME")
 
 
 def store_interview_to_s3(user_id, interview_data):
     s3_key = f"interviews/{user_id}.json"
 
     try:
-        response = s3.get_object(Bucket=S3_BUCKET_NAME, Key=s3_key)
+        response = s3.get_object(Bucket=bucket_name, Key=s3_key)
         existing_data = json.loads(response["Body"].read().decode("utf-8"))
     except s3.exceptions.NoSuchKey:
         existing_data = []
